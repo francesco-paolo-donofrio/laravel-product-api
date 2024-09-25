80cf874a04a223925df088cebc6997ba879bd6c5
Frankosviz <geom.francescopaolo@gmail.com>
2024-09-25 23:06:03 +0200

    Fixing problem with migrations

--- database/migrations/2024_09_25_185203_create_categories_table.php
+++ database/migrations/2024_09_25_185203_create_categories_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('name', length: 150);
             $table->timestamps();
         });
     }
