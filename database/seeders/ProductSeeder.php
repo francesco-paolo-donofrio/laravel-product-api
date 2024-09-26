25224dfb6d32c4ac05374269740c5644d7e81947
Frankosviz <geom.francescopaolo@gmail.com>
2024-09-26 18:32:21 +0200

    Setting the faker

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Models\Product;
+
 
 class ProductSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class ProductSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $faker = Faker::create();
+        for ($i = 0; $i < 100; $i++) {
+            Product::create([
+                'name' => $faker->word(),
+                'description' => $faker->sentence(),
+                'price' => $faker->randomFloat(2, 10, 100), // Price between 10 and 100
+            ]);
+        }
     }
 }
