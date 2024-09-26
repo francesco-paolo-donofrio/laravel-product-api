267a053acf4ac6ab40e7a4e87ef5178c00163bb2
Frankosviz <geom.francescopaolo@gmail.com>
2024-09-26 19:14:08 +0200

    Api Controllers - Moving

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +15,22 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+
+Route::get('/products', function (Request $request) {
+    return Product::all();
+});
+
+Route::get('/categories', function (Request $request) {
+    return Category::all();
+});
+
+Route::get('/categories/{category}/products', function (Request $request, Category $category) {
+    return $category->products;
+});
+
+Route::get('/products/{product}/category', function (Request $request, Product $product) {
+    return $product->category;
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
